14c29b6b64ed731564c174dd4d9796fcceff4860
stevejj4 <jumaomondi16@gmail.com>
2024-06-16 18:15:12 +0300

    Database dependancy

--- README.md
+++
@@ -1,2 +0,0 @@
-# Data-Ingestion
-This has code to ingest data from an excel file
--- src/main/java/example/DataBaseUtils.java
+++ src/main/java/example/DataBaseUtils.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DataBaseUtils {
     private static final String URL = "jdbc:postgresql://localhost:5432/Trial";
     private static final String USER = "Postgre";
-    private static final String PASSWORD = "SHOFCO@2024";
+    private static final String PASSWORD = "@07SteveJJ";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
